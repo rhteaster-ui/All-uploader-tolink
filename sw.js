3099747cf34cabbb1bbaaeff90689dfe06be2b91
rhteaster-ui <rhteaster@gmail.com>
2026-05-27 12:15:05 +0700

    Delete file via RepoFlow: sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE = 'uploader-v1';
-const ASSETS = ['/', '/index.html', '/src/app.js', '/manifest.webmanifest', '/src/icon.png'];
-self.addEventListener('install', (e) => e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS))));
-self.addEventListener('fetch', (e) => e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request))));
